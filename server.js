37c24df0ba7d6a805f5f31feffc459d40c52fdf1
bobburgurs <122063291+bobburgurs@users.noreply.github.com>
2023-10-10 20:18:08 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const app = express();
 
-// Serve static files from the `public` directory
-app.use(express.static("./public"));
+// Serve static files from the "public" folder
+app.use(express.static("public"));
 
-// Handle GET requests to the `/` endpoint
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+// Handle GET request
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/src/pages/index.html");
 });
 
-// Handle POST requests to the `/` endpoint
-app.post("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index2.html");
+// Handle POST request
+app.post("/", function (req, res) {
+  res.sendFile(__dirname + "/src/pages/index2.html");
 });
 
-// Start the server on port 3000
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// Listen on port 3000
+app.listen(3000, function () {
+  console.log("Server is running on port 3000.");
 });
